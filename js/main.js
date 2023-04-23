8956365ab2f10ce60968a49a178ad998d71a0126
taielmatiasdeluca <taielmatiasdeluca@gmail.com>
2023-04-23 20:47:57 -0300

    se arreglaron errores

--- js/main.js
+++ js/main.js
@@ -1,36 +1,31 @@
 
-const project = {
+const project = 
+{
     name:'',
     img:'',
     link:'',
     descripcion:''
 }
 
-
 let newProject = (name,img,descripcion,link)=>{
-    let proyecto = project;
+    let proyecto = {...project};
     proyecto.name = name;
     proyecto.img = img;
     proyecto.link = link;
-    
     proyecto.descripcion = descripcion;
     return proyecto;
 }
 
 let projects  = [
-    newProject('TP1','none.png','Trabajo Numero 1','/TP1/'),
-    newProject('TP2','none.png','Trabajo Numero 2','/TP2/'),
-    
+    newProject('TP1','none.png','Trabajo Numero 1','TP1/index.html'),
+    newProject('TP2','none.png','Trabajo Numero 2','TP2/index.html'),
 ]
 
-let tpl = project_main_tpl.content.cloneNode(true);
 projects.forEach(proyecto => {
-    
-    let proyectoHtml = tpl.cloneNode(true);
+    let proyectoHtml = project_main_tpl.content.cloneNode(true);
     proyectoHtml.querySelector('.project__name').textContent = proyecto.name;
     proyectoHtml.querySelector('.project_image').textContent = proyecto.img;
     proyectoHtml.querySelector('.project__descripcion').textContent = proyecto.descripcion;
     proyectoHtml.querySelector('a').href = proyecto.link;
-    
     project__list.appendChild(proyectoHtml);
 });
